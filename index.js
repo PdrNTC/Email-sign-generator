142153a7f8920746c95cdadb024557d5aad7febe
Pedro Tashima <pobis31@gmail.com>
2020-05-23 20:15:33 -0300

    merged with diego(not working)

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+var inputNome;
+
+function gerarAssinatura(){
+  inputNome = document.querySelector('input[name=nome]').value;
+
+}
 
 
+var teste = 'Azul';
+
+
+
+var linkElement = document.createElement('a');
+linkElement.setAttribute('tr', teste);
+
+var textElement = document.createTextNode(teste);
+linkElement.appendChild(textElement);
+
+var containerElement = document.querySelector('body div#info');
+containerElement.appendChild(linkElement);
