454be574ea259d6bd235a4ffcbce0989f28961ad
Pedro Tashima <pobis31@gmail.com>
2020-05-24 01:41:32 -0300

    mudanças no modelo de assinatura
    O modelo da assinatura foi alterado, o campo de nome e sobrenome foram separados.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const buttonGerar = document.querySelector('#btn');
 
 const nomeInput = document.querySelector('.forms #nome');
+const sobrenomeInput = document.querySelector('.forms #sobrenome');
 const celularInput = document.querySelector('.forms #cel');
 const emailInput = document.querySelector('.forms #email');
 const cargosInput = document.querySelector("#cargos");
 
 const nomeField = document.getElementById('nome-assinatura');
+const sobrenomeField = document.getElementById('sobrenome-assinatura');
 const celularField = document.getElementById('numero-assinatura');
 const emailField = document.getElementById('email-assinatura');
 const cargoField = document.getElementById('cargo-assinatura');
@@ -17,16 +19,19 @@ console.log({ value: value, text: text});
 function btn() {
 
   var nome = nomeInput.value;
+  var sobrenome = sobrenomeInput.value;
   var celular = celularInput.value;
   var email = emailInput.value;
   var cargo = cargosInput.options[cargosInput.selectedIndex].value;
   var info = {
     nome: nome,
+    sobrenome: sobrenome,
     celular: celular,
     email: email,
     cargo: cargo
   }
   nomeField.innerHTML = nome;
+  sobrenomeField.innerHTML = sobrenome;
   celularField.innerHTML = celular;
   emailField.innerHTML = email;
   cargoField.innerHTML = cargo;
