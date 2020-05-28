afaf2e5f04c637ce08d49ed2f295e9d25d78013e
Pedro Tashima <pobis31@gmail.com>
2020-05-27 21:32:07 -0300

    identação e comentários adicionados

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-// Encontra o botao escrito Gerar Assinatura e define qual a função queserá executada quando este for pressionado
+// Encontra o botao escrito Gerar Assinatura e define qual a funÃ§Ã£o queserÃ¡ executada quando este for pressionado
 const buttonGerar = document.getElementById('btn-gerar')
 buttonGerar.onclick = btn;
 
@@ -9,23 +9,23 @@ const celularInput = document.getElementById('cel');
 const emailInput = document.getElementById('email');
 const cargosInput = document.getElementById('cargos');
 
-// Encontra todas as areas que serão preenchidas
+// Encontra todas as areas que serÃ£o preenchidas
 // (os campos na assinatura)
 const nomeCompletoField = document.getElementById('nome-assinatura');
 const celularField = document.getElementById('numero-assinatura');
 const emailField = document.getElementById('email-assinatura');
 const cargoField = document.getElementById('cargo-assinatura');
 
-// Define a função que será executada quando o botão for pressionado
+// Define a funÃ§Ã£o que serÃ¡ executada quando o botÃ£o for pressionado
 function btn() {
-  // 1 - Salva as informações em variaveis
+  // 1 - Salva as informaÃ§Ãµes em variaveis
   var nome = nomeInput.value;
   var sobrenome = sobrenomeInput.value;
   var celular = celularInput.value;
   var email = emailInput.value;
   var cargo = cargosInput.options[cargosInput.selectedIndex].value;
 
-  // 2 - Edita algumas das entradas, pra manter um padrão
+  // 2 - Edita algumas das entradas, pra manter um padrÃ£o
   let nomeCompleto = nome.trim() + ' ' + sobrenome.trim()
   let celularEditado = editaCelular(celular.trim())
   let emailEditado = checkEmail(email);
@@ -36,28 +36,28 @@ function btn() {
   emailField.innerHTML = emailEditado;
   cargoField.innerHTML = cargo;
   
-  // 4 - Preenche as informações de links nos campos necessários
+  // 4 - Preenche as informaÃ§Ãµes de links nos campos necessÃ¡rios
   celularField.setAttribute('href', `tel:${celularEditado}`);
   emailField.setAttribute('href', `mailto:${email}`);
 }
 
-// Define função que deixa as primeiras letras do nome Maiusculas
+// Define funÃ§Ã£o que deixa as primeiras letras do nome Maiusculas
 function primeirasMaiusculas(nome) {
   try {
 
-    // Primeiro separamos o nome completo em partes (que são separadas pelos espaços)
+    // Primeiro separamos o nome completo em partes (que sÃ£o separadas pelos espaÃ§os)
     // ex: diego ferreira - se torna - [diego] [ferreira]
     sentence = nome.toLowerCase().split(" ");
-    // Depois iteramos em cada pedaço
+    // Depois iteramos em cada pedaÃ§o
     // Pegando a letra 1 (index = 0) e transformando em maiuscula
-    // Depois juntando o resto do pedaço
+    // Depois juntando o resto do pedaÃ§o
     // ex: [diego] -> d | iego -> D | iego -> [Diego]
     for (let i = 0; i < sentence.length; i++) {
       let primeiraLetra = sentence[i][0].toUpperCase();
       let resto = sentence[i].slice(1); // slice nesse caso retorna o resto, depois da letra 1
       sentence[i] = primeiraLetra + resto;
     }
-    // Por fim, retornamos a junçao dos pedaços
+    // Por fim, retornamos a junÃ§ao dos pedaÃ§os
     return sentence.join(" ");
   } catch(err) {
     return ''
@@ -66,7 +66,7 @@ function primeirasMaiusculas(nome) {
 
 function editaCelular(cel) {
   try {
-    // 1 - Retira os espaços e simbolos
+    // 1 - Retira os espaÃ§os e simbolos
     cel = cel.replace(/ /g, '');
     cel = cel.replace(/\(/g, '');
     cel = cel.replace(/\)/g, '');
@@ -79,19 +79,19 @@ function editaCelular(cel) {
       editado = cel.slice(0, 2) + ' ' + cel.slice(2, 3) + '' + cel.slice(3, 7) + ' ' + cel.slice(7)
       return editado
     } else {
-      // Senão, mostra um alerta de erro
+      // SenÃ£o, mostra um alerta de erro
       alert('Coloque o telefone no formato 12 12345 1234 (com 11 numeros)');
       return ''
     }
   } catch(err) {}
 }
 
-// Define função que confere se o email é da solucaut
+// Define funÃ§Ã£o que confere se o email Ã© da solucaut
 function checkEmail(email) {
   if (email.includes('@solucaut.com.br')) {
     return email;
   } else {
-    alert('Você não está usando o email da Solucaut...');
+    alert('VocÃª nÃ£o estÃ¡ usando o email da Solucaut...');
     return ''
   }
 }
